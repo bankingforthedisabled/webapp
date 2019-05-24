782aa305ba80593362d25d0ad991917df124b9f8
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-05-23 21:56:11 -0400

    fixed home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -22,23 +22,27 @@ const Home = () => (
             <div className="clearfix"></div>
           </div>
         </div>
-        <div className="account-entry">
-          <h3 className="account-name">Checking</h3>
-          <h3 className="account-amount">$25.00</h3>
-          <div className="clearfix" />
+        <div className="account-details">
+          <h2 className="account-details-header">Loan Status</h2>
+          <div className="account-entry">
+            <h3 className="account-name">Balance Remaining</h3>
+            <h3 className="account-amount">$200</h3>
+            <div className="clearfix"></div>
+          </div>
+          <div className="account-entry">
+            <h3 className="account-name">Next Payement Due</h3>
+            <h3 className="account-amount">11/15/2019</h3>
+            <div className="clearfix"></div>
+          </div>
+          <div className="voice-actions">
+            <h3>Voice Actions</h3>
+            <p>Voice instructions here...</p>
+          </div>
         </div>
       </div>
       <RightSidebar />
     </div>
-    <div className="right-sidebar">
-      <div className="deposit center-box">
-        <h3 className="center-box-title">Make a Payment</h3>
-      </div>
-      <div className="transfer center-box">
-        <h3 className="center-box-title">Transfer</h3>
-      </div>
-    </div>
-  </div>
+  </div >
 );
 
 export default Home;
