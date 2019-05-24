fabdb520241db5bdae026daed34be79db17712c0
Deepak Goel <goel.deepak9@gmail.com>
2019-05-24 00:43:32 -0400

    API working w react elements

--- src/components/LeftSidebar/LeftSidebar.js
+++ src/components/LeftSidebar/LeftSidebar.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const LeftSidebar = () => (
   <div className="left-sidebar">
     <h3>Loading...</h3>
     <p>Hold still to calibrate!</p>
-    <div className="payments"></div>
+    <div className="payments" />
+    <div className="voice-actions">
+      <h1>Voice Actions</h1>
+      <p>Voice instructions here...</p>
+    </div>
   </div>
-)
+);
 
-export default LeftSidebar
+export default LeftSidebar;
--- src/lib/nessie.js
+++ src/lib/nessie.js
@@ -1,22 +1,29 @@
 import axios from "axios";
+import { async } from "q";
 
 const account_number = "5ce3fb8c322fa06b67794db6";
-const api_key = process.env.REACT_APP_NESSIE_KEY;
-// const customer_id = "5ce3fb8b322fa06b67794db1";
+const api_key = "7e9e0606ab9d8df00f3622753349bc63";
+const customer_id = "5ce3fb8b322fa06b67794db1";
 
 //getCustomer - Returns an JSON object of key value pairs (data.first_name, data.last_name, data.address)
 export const getCustomerData = () => {
-  return axios.get(`http://api.reimaginebanking.com/accounts/${account_number}/customer?key=${api_key}`)
+  return axios
+    .get(
+      `http://api.reimaginebanking.com/accounts/${account_number}/customer?key=${api_key}`
+    )
     .then(res => {
-      console.log(res.data);
+      return res.data;
     });
 };
 
 //getLoans - Returns an array of loans associated with the account
 export const getCustomerLoans = () => {
-  return axios.get(`http://api.reimaginebanking.com/accounts/${account_number}/customer?key=${api_key}`)
+  return axios
+    .get(
+      `http://api.reimaginebanking.com/accounts/${account_number}/customer?key=${api_key}`
+    )
     .then(res => {
-      console.log(res);
+      return res;
     });
 };
 
@@ -25,19 +32,25 @@ export const getCustomerLoans = () => {
 // data.transaction_date, data.type, data.status, data.payer_id, data.payee_id, data.amount
 // for payer and payee id, call getCustomer (only print first name)
 export const getCustomerTransfers = () => {
-  return axios.get(`http://api.reimaginebanking.com/accounts/${account_number}/customer?key=${api_key}`)
+  return axios
+    .get(
+      `http://api.reimaginebanking.com/accounts/${account_number}/customer?key=${api_key}`
+    )
     .then(res => {
-      console.log(res);
+      return res;
     });
 };
 
 //getAccounts
 // Returns array of accounts
 /* for(let k in res.data) { data[k].type, data[k].balance }
-  */
+ */
 export const getCustomerAccounts = () => {
-  return axios.get(`http://api.reimaginebanking.com/accounts/${account_number}/customer?key=${api_key}`)
+  return axios
+    .get(
+      `http://api.reimaginebanking.com/customers/${customer_id}/accounts?key=${api_key}`
+    )
     .then(res => {
-      console.log(res.data);
+      return res.data;
     });
 };
