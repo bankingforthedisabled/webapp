b49a4506ba658d7661f18846017e970e2f3422ce
Deepak Goel <goel.deepak9@gmail.com>
2019-05-24 02:39:32 -0400

    Make payment changed to loans, linked to loan page

--- src/components/RightSidebar/RightSidebar.js
+++ src/components/RightSidebar/RightSidebar.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Redirect, Link } from "react-router-dom";
 
 const RightSidebar = () => (
   <div className="right-sidebar">
     <div className="deposit center-box">
-      <h3 className="center-box-title">Make a Payment</h3>
+      <Link to="/LoanList">
+        <h3 className="center-box-title">Loans</h3>
+      </Link>
     </div>
     <div className="transfer center-box">
       <h3 className="center-box-title">Transfer</h3>
     </div>
   </div>
-)
+);
 
-export default RightSidebar
+export default RightSidebar;
